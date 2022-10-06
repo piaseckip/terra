bd2e0bad54dc34eae48beba4f4b30f183a1e47f8
Piotr Piasecki <piterpiasecki@gmail.com>
2022-10-06 18:09:34 +0200

    changes in mian

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "Compute" {
   micro              = var.micro
   ami_ubuntu         = var.ami_ubuntu
   ssh_key            = var.ssh_key
-  docker_config_file = var.docker-config-file
+  docker_config_file = var.docker_config_file
   name               = var.name
   bootcamp           = var.bootcamp
   ec2_name           = var.ec2_name
@@ -20,12 +20,7 @@ module "Network" {
   region             = var.region
   zoneA              = var.zoneA
   zoneB              = var.zoneB
-  micro              = var.micro
-  ami_ubuntu         = var.ami_ubuntu
-  ssh_key            = var.ssh_key
-  docker_config_file = var.docker_config_file
   name               = var.name
   bootcamp           = var.bootcamp
-  ec2_name           = var.ec2_name
   tags               = var.tags
 }
