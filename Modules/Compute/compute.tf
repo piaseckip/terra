37788c489394d3dc2d99eeeb87afb688358afaf7
Piotr Piasecki <piterpiasecki@gmail.com>
2022-10-06 17:59:55 +0200

    changed

--- Modules/Compute/compute.tf
+++ Modules/Compute/compute.tf
@@ -1,31 +1,31 @@
 resource "aws_instance" "instance1" {
-  ami                    = var.ami-ubuntu
+  ami                    = var.ami_ubuntu
   instance_type          = var.micro
   availability_zone      = var.zoneA
   subnet_id              = aws_subnet.my_subnet.id
-  key_name               = var.ssh-key
-  user_data              = file(var.docker-config-file)
+  key_name               = var.ssh_key
+  user_data              = file(var.docker_config_file)
   vpc_security_group_ids = [aws_security_group.tf-alb_sg.id]
-  tags                   = {
-    name = "${var.ec2_name}${1}"
+  tags = {
+    name       = "${var.ec2_name}${1}"
     created_by = "Piotr Piasecki"
     bootcamp   = "poland1"
-  } 
-  volume_tags            = var.tags
+  }
+  volume_tags = var.tags
 }
 
 resource "aws_instance" "instance2" {
-  ami                    = var.ami-ubuntu
+  ami                    = var.ami_ubuntu
   instance_type          = var.micro
   availability_zone      = var.zoneB
   subnet_id              = aws_subnet.my_subnet2.id
-  key_name               = var.ssh-key
-  user_data              = file(var.docker-config-file)
+  key_name               = var.ssh_key
+  user_data              = file(var.docker_config_file)
   vpc_security_group_ids = [aws_security_group.tf-alb_sg.id]
-  tags                   = {
-    name = "${var.ec2_name}${2}"
+  tags = {
+    name       = "${var.ec2_name}${2}"
     created_by = "Piotr Piasecki"
     bootcamp   = "poland1"
-  } 
-  volume_tags            = var.tags
+  }
+  volume_tags = var.tags
 }
--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
 module "Compute" {
-  source = "./Modules/Compute"
-  depends_on = [module.Network]
-  region = var.region
-  zoneA = var.zoneA
-  zoneB = var.zoneB
-  micro = var.micro
-  ami-ubuntu = var.ami-ubuntu
-  ssh-key = var.ssh-key
-  docker-config-file = var.docker-config-file
-  name = var.name
-  bootcamp = var.bootcamp
-  ec2_name = var.ec2_name
-  tags = var.tags
+  source             = "./Modules/Compute"
+  depends_on         = [module.Network]
+  region             = var.region
+  zoneA              = var.zoneA
+  zoneB              = var.zoneB
+  micro              = var.micro
+  ami_ubuntu         = var.ami_ubuntu
+  ssh_key            = var.ssh_key
+  docker_config_file = var.docker-config-file
+  name               = var.name
+  bootcamp           = var.bootcamp
+  ec2_name           = var.ec2_name
+  tags               = var.tags
 
-  
+
 }
 module "Network" {
-  source = "./Modules/Network"
-  region = var.region
-  zoneA = var.zoneA
-  zoneB = var.zoneB
-  micro = var.micro
-  ami-ubuntu = var.ami-ubuntu
-  ssh-key = var.ssh-key
-  docker-config-file = var.docker-config-file
-  name = var.name
-  bootcamp = var.bootcamp
-  ec2_name = var.ec2_name
-  tags = var.tags
+  source             = "./Modules/Network"
+  region             = var.region
+  zoneA              = var.zoneA
+  zoneB              = var.zoneB
+  micro              = var.micro
+  ami_ubuntu         = var.ami_ubuntu
+  ssh_key            = var.ssh_key
+  docker_config_file = var.docker_config_file
+  name               = var.name
+  bootcamp           = var.bootcamp
+  ec2_name           = var.ec2_name
+  tags               = var.tags
 }
