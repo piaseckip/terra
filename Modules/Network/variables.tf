e3e0bdd48b61d5673d2bdb321858521417a04ae0
Piotr Piasecki <piterpiasecki@gmail.com>
2022-10-06 18:39:48 +0200

    fix

--- Modules/Network/variables.tf
+++ Modules/Network/variables.tf
@@ -10,29 +10,12 @@ variable "zoneB" {
   description = "Second AZ of region"
 }
 
-variable "micro" {
-  description = "Micro - size of ec2 aws instance"
-}
-
-variable "ami_ubuntu" {
-  description = "Default ami of ubuntu image"
-}
-
-variable "ssh_key" {
-  description = "Main key used for ec2 instances"
-}
-
-variable "docker_config_file" {
-  description = "File with docker configuration"
-}
 variable "name" {
   description = "Value for created_by tag"
 }
+
 variable "bootcamp" {
   description = "Value for bootcamp tag"
-}
-variable "ec2_name" {
-  description = "Default name for ec2 instance"
 }
 
 variable "tags" {
